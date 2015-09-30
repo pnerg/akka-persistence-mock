5326c013f96b6bf8c4f4fbd84dc60cb07ea2522a
Peter Nerg <peter.nerg@gmail.com>
2015-09-30 15:11:56 +0200

    #1 #2 #3 #4 Added JournalPlugin

---
+++ src/main/scala/org/dmonix/akka/persistence/JournalPlugin.scala
@@ -0,0 +1,46 @@
+/**
+ *  Copyright 2015 Peter Nerg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.dmonix.akka.persistence
+
+import scala.collection.immutable.Seq
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Try
+import akka.persistence.{ AtomicWrite, PersistentRepr }
+import akka.persistence.journal.{ AsyncRecovery, AsyncWriteJournal }
+
+/**
+ * @author Peter Nerg
+ */
+class JournalPlugin {
+
+  def asyncWriteMessages(messages: Seq[AtomicWrite]): Future[Seq[Try[Unit]]] = {
+    ???
+  }
+
+  def asyncDeleteMessagesTo(persistenceId: String, toSequenceNr: Long): Future[Unit] = {
+    ???
+  }
+
+  def asyncReplayMessages(persistenceId: String, fromSequenceNr: Long, toSequenceNr: Long,
+                          max: Long)(recoveryCallback: PersistentRepr ⇒ Unit): Future[Unit] = {
+
+    ???
+  }
+
+  def asyncReadHighestSequenceNr(persistenceId: String, fromSequenceNr: Long): Future[Long] = {
+    ???
+  }
+}
