47cc0563f0e52a3fc6c29c655b926ff8f38db8e6
Peter Nerg <peter.nerg@gmail.com>
2015-10-01 11:08:58 +0200

    add AccountActor

---
+++ src/test/scala/org/dmonix/akka/persistence/AccountActor.scala
@@ -0,0 +1,87 @@
+/**
+ *  Copyright 2015 Peter Nerg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.dmonix.akka.persistence
+
+import akka.persistence.PersistentActor
+import akka.actor.ActorLogging
+import akka.actor.Props
+import akka.persistence.SnapshotOffer
+
+case class Deposit(sum: Integer)
+case class Withdraw(sum: Integer)
+case object Balance
+case object Snap
+
+object Account {
+  def apply(balance: Integer) = new Account(balance)
+}
+
+class Account(balance: Integer) {
+
+  def deposit(sum: Integer) = {
+    Account(balance + sum)  
+  }
+
+  def withdraw(sum: Integer) = {
+    Account(balance - sum)  
+  }
+  
+  def balance(): Integer = balance
+  
+  
+  override def toString = String.valueOf(balance)
+}
+
+/**
+ * @author Peter Nerg
+ */
+object AccountActor {
+  def props(id: String) = Props(new AccountActor(id))
+}
+
+/**
+ * @author Peter Nerg
+ */
+class AccountActor(id: String) extends PersistentActor with ActorLogging {
+  override def persistenceId = id
+  var account = Account(0)
+  
+  private def deposit(evt: Deposit) {
+    account = account.deposit(evt.sum)  
+  }
+
+  private def withdraw(evt: Withdraw) {
+    account = account.withdraw(evt.sum)
+  }
+  
+  val receiveRecover: Receive = {
+    case evt: Deposit => deposit(evt)
+    case evt: Withdraw => withdraw(evt)
+    case SnapshotOffer(_, snapshot: Account) => {
+      log.debug("Restoring snapshot [{}] for [{}]", snapshot, persistenceId)
+      account = snapshot
+    }
+    case x: Any => log.debug("Got message [{}]", x)
+  }
+
+  val receiveCommand: Receive = {
+    case evt: Deposit => persist(evt)(deposit)
+    case evt: Withdraw => persist(evt)(withdraw)
+    case Balance => log.debug("Balance [{}]", account.balance)
+    case Snap => saveSnapshot(account)
+    case x: Any => log.debug("Got message [{}]", x)
+  }
+}
